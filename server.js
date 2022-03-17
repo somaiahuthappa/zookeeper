285412f5a35c3b96b7fb1b9d1326c28b8bafa3f0
Som's Laptop <somaiahuthappa@gmail.com>
2022-03-17 19:50:26 -0400

    added port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
 const { animals } = require('./data/animals');
